20fad50650e7113b952a328de0547d72c48963cf
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-03-10 14:53:58 +0300

    Created 3 function, and used them.

--- Week2/01.cpp
+++ Week2/01.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
+#define SIZE 5
+void initializeArray(int[], int);
+void printArray(int[], int);
+void updateZero(int[], int);
 
 int main(void) {
 
 
+	int myArray[SIZE] = { 0 };
+
+	initializeArray(myArray, 5);
+	printArray(myArray, 5);
+	updateZero(myArray, 5);
+	printArray(myArray, 5);
 
 
 
@@ -12,7 +22,37 @@ int main(void) {
 
 
 
-
-
+	cout << "\n\n\n";
+	system("pause");
 	return 0;
+}
+
+void initializeArray(int array[], int size) {
+
+
+	for (int i = 0; i < size; i++) {
+		cout << "Please enter variable " << i+1 << ": ";
+		cin >> array[i];
+	}
+	cout << "\n\n";
+}
+
+void printArray(int array[], int size) {
+
+	for (int i = 0; i < size; i++) {
+
+		cout << "Element in index " << i << " is " << array[i]<<endl;
+	}
+	cout << "\n\n";
+
+}
+
+void updateZero(int array[], int size) {
+
+	for (int i = 0; i < size; i++) {
+		array[i] = 0;
+	}
+	string message = "-----------All values are updated to 0.-----------\n\n";
+	cout << message;
+
 }

4a1f25e0e33cd3fb117b11626311c56aa0e4bb12
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-03-10 16:00:33 +0300

    More example covered.

--- Week2/01.cpp
+++ Week2/01.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
-#define SIZE 5
+#define SIZE 2
 void initializeArray(int[], int);
 void printArray(int[], int);
 void updateZero(int[], int);
+void reverseArray(int [], int size);
+void initializeRandomArray(int [], int size);
 
 int main(void) {
 
-
-	int myArray[SIZE] = { 0 };
-
-	initializeArray(myArray, 5);
-	printArray(myArray, 5);
-	updateZero(myArray, 5);
-	printArray(myArray, 5);
+	int myArray[10] = { 0 }; 
 
 
+	//initializeArray(myArray, SIZE);
+	//printArray(myArray, SIZE);
+	
+	//updateZero(myArray, SIZE);
+	//printArray(myArray, SIZE);
+
+	initializeRandomArray(myArray, 10);
+	printArray(myArray, 10);
+	
+	reverseArray(myArray, 10);
+	printArray(myArray, 10);
 
 
-
-
+	
 
 
 	cout << "\n\n\n";
@@ -36,6 +43,16 @@ void initializeArray(int array[], int size) {
 	}
 	cout << "\n\n";
 }
+void initializeRandomArray(int array[], int size) {
+
+	srand(time(NULL));
+
+	for (int i = 0; i < size; i++) {
+
+		array[i] = 100 + rand() %1000;
+	}
+
+}
 
 void printArray(int array[], int size) {
 
@@ -46,7 +63,6 @@ void printArray(int array[], int size) {
 	cout << "\n\n";
 
 }
-
 void updateZero(int array[], int size) {
 
 	for (int i = 0; i < size; i++) {
@@ -55,4 +71,17 @@ void updateZero(int array[], int size) {
 	string message = "-----------All values are updated to 0.-----------\n\n";
 	cout << message;
 
+}
+void reverseArray(int array[], int size) {
+	
+	int temp;
+	int j = size - 1;
+	for (int i = 0; i < j; i++, j--)
+	{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+	}
+
+	cout << "-------------ARRAY REVERSED!-------------\n\n";
 }

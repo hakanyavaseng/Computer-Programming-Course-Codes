e0f7d2497512ae3a110625be4018323f8c6a30b4
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-03-17 14:42:31 +0300

    2D array initilization and printing out.

--- Week3/01.cpp
+++ Week3/01.cpp
@@ -1,26 +1,35 @@
-//POINTERS
-#include <stdio.h>
+//Multidimensional Arrays Examples
+#include <iostream>
 using namespace std;
+
+
 
 int main(void) {
 
+	int myArray[3][3] = {0};
 
 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "Please enter elements: ";
+			cin >> myArray[i][j];
+
+		}
+	}
 
 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << myArray[i][j];
 
+		}
 
-
-
-
-
-
-
-
-
-
-
-
+		cout << endl;
+	}
 
 
 	return 0;

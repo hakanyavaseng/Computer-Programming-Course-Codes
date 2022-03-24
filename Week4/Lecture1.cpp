42d8c297b9ca59414acee50443ce7e96cf2b6928
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-03-24 14:41:00 +0300

    Char pointer using cout and printf.

--- Week4/Lecture1.cpp
+++ Week4/Lecture1.cpp
@@ -1,17 +1,22 @@
 //24.03.2022 - WEEK 4 - COMPUTER PROGRAMMING COURSE
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
 
 
 int main(void) {
 
 
+	char a = 'x';
+	char* ptrA = &a;
+	
+	// cout << &a << endl; ------ Cout gives error while using char pointer because of overloading on cout function.
+
+	printf("%p", ptrA);
 
 
-
-
-
-
+	
 
 
 

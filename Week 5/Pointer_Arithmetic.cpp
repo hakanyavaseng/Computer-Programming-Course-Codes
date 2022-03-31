8945ea715600782ddc680493cdd8a31127261574
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-03-31 14:43:22 +0300

    Subtracting 2 pointers.

--- Week 5/Pointer_Arithmetic.cpp
+++ Week 5/Pointer_Arithmetic.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-	int A[10] = {0,1,2,3,4,5,6,7,8,9};
+	int A[10] = {0,1,2,3,150,5,6,7,8,9};
 	int* ptrA = &A[0];
 
 	cout << "Address of A: " << &A << "\nAddress of ptrA: " << ptrA << endl;
@@ -16,8 +16,14 @@ int main() {
 	cout << "A[1] = " << A[1] << endl;
 	cout << "Address of ptrA + 1: " << ptrA << "\nValue of ptrA + 1: " << *ptrA << endl;
 
+	int* ptrB = &A[5];
 
+	cout << "\n\n";
 
+	cout << "ptrB = " << ptrB << "\nptrA = " << ptrA << endl;
+	cout << "ptrB - ptrA = "<< ptrB - ptrA << endl; // Aradaki byte farkını değil, türe göre kaç birim geri gitmesi gerektiğini gösteriyor.
+
+       
 
 
 

caef4099c7db61e40518e4df516ea2e11211cb88
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-04-07 15:21:32 +0300

    Some examples.

--- Week 6/Lecture1.cpp
+++ Week 6/Lecture1.cpp
@@ -4,22 +4,51 @@ using namespace std;
 int main(void) {
 
 
-	int myArray[3][6] = { {10,20,30,40,50,60}, {70,80,90,100,110,120} , {130,140,150,160,170,180} };
+	int myArray[3][6] = { {1,2,3,4,5,6}, {7,8,9,10,11,12}, {13,14,15,16,17,18} };
 	int index = 0;
 	
 	cout << "Address of myArray: " << myArray << endl;
 	cout << "Address of myArray: " << myArray[0] << endl;
 	cout << "Address of M[0][0]: " << &myArray[0][0] << endl;
 
+	/*int sum = 0;
 
-	/*for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 6; j++) {
 
-			cout << *(&myArray[0][0] + index) << " ";
+			cout << *(&myArray[0][0] + index) << "\t";
 			index++;
 		}
 		cout << endl;
-	}*/
+	}
+
+	cout << "\n\n"; 
+
+	index = 0;
+
+	for (int i = 0; i < 3 * 6; i++) {
+
+		sum += *(&myArray[0][0] + i);
+	    index++;
+
+
+	}
+		cout << "Sum is: " << sum;
+
+	*/
+
+	cout << myArray[1][1] << " " << *(myArray + 7)<<endl; // Beklendiği gibi çalışmıyor. Hoca link atacak.
+	cout << myArray[1][1] << " " << *(myArray[0] + 7)<<endl;
+
+
+
+
+
+
+
+	
+
+
 
 
 

8c6d31c43424beb5e2aeb82e7cacf8f58ab8e0cf
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-04-07 14:34:32 +0300

    MD Arrays and pointers.

--- Week 6/Lecture1.cpp
+++ Week 6/Lecture1.cpp
@@ -4,6 +4,22 @@ using namespace std;
 int main(void) {
 
 
+	int myArray[3][6] = { {10,20,30,40,50,60}, {70,80,90,100,110,120} , {130,140,150,160,170,180} };
+	int index = 0;
+	
+	cout << "Address of myArray: " << myArray << endl;
+	cout << "Address of myArray: " << myArray[0] << endl;
+	cout << "Address of M[0][0]: " << &myArray[0][0] << endl;
+
+
+	/*for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 6; j++) {
+
+			cout << *(&myArray[0][0] + index) << " ";
+			index++;
+		}
+		cout << endl;
+	}*/
 
 
 

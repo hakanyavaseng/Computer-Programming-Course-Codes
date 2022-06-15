9018e4a67b08838f4575b8ba5203907baa1fbc07
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-15 18:25:07 +0300

    An example.

--- DMA/DMA.cpp
+++ DMA/DMA.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(void)
 {
 
-	int var1 = 10;
+	// Working on heap and stack, watched memory allocation.
+
+	/*int var1 = 10;
 	int myArray[5];
 
 	myArray[0] = 1;
@@ -24,9 +26,29 @@ int main(void)
 	harray[1] = 2;
 	harray[2] = 3;
 	harray[3] = 4;
-	harray[4] = 5;
+	harray[4] = 5;*/
 
+	int* count, * arrayptr;
+	count = new int;
 
+	cout << "How many students? ";
+	cin >> *count;
+
+	arrayptr = new int[*count];
+
+	for (int i = 0; i < *count; i++)
+	{
+		cout << "Enter score " << i+1 << ": ";
+		cin >> arrayptr[i];
+	}
+
+	for (int i = 0; i < *count; i++)
+	{
+		cout << "Student " << i+1 << ": " << arrayptr[i] << endl;
+	}
+
+	delete count;
+	delete[] arrayptr;
 
 
 

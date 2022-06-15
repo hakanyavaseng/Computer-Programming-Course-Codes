853d57b2c382207b5f14611ff8fa2b846bfef1c8
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-15 18:13:21 +0300

    Working on stack and heap.

--- DMA/DMA.cpp
+++ DMA/DMA.cpp
@@ -5,11 +5,26 @@ using namespace std;
 int main(void)
 {
 
+	int var1 = 10;
+	int myArray[5];
+
+	myArray[0] = 1;
+	myArray[1] = 2;
+	myArray[2] = 3;
+	myArray[3] = 4;
+	myArray[4] = 5;
 
 
+	int* hvar = new int;
+	*hvar = 7;
 
+	int* harray = new int[5];
 
-
+	harray[0] = 1;
+	harray[1] = 2;
+	harray[2] = 3;
+	harray[3] = 4;
+	harray[4] = 5;
 
 
 

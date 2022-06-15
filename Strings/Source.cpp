2c7596a53042316d3419e96b114f77c74e450501
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-15 13:15:37 +0300

    String input.

--- Strings/Source.cpp
+++ Strings/Source.cpp
@@ -10,15 +10,36 @@ int main(void)
 
 	// Stringi print etmek!
 
-	puts(str1); // Otomatik olarak boþluk ekler.
-	printf("%s\n", str1); // Alta geçmesi için new line gerekiyor
-	cout << str1 << endl; // new line atmak gerekiyor
+	//puts(str1); // Otomatik olarak boþluk ekler.
+	//printf("%s\n", str1); // Alta geçmesi için new line gerekiyor
+	//cout << str1 << endl; // new line atmak gerekiyor
 
 	// Printf ile string formatlama 
 	
-	printf("%40s\n", str1); 
+	/*printf("%40s\n", str1); 
 	printf("%-40s bosluk sonrasi gelen yazi\n", str1);
-	printf("%40.10s\n", str1);
+	printf("%40.10s\n", str1);*/
+
+	// String input.
+
+	char str2[50];
+	cout << "Please enter a string up to 49 characters: ";
+	
+	// gets(str2); Dangerous to use and removed from C++.
+    
+	cin >> str2; // Boþluklarý almýyor.
+	printf("%s\n", str2);
+
+	cin.clear();
+	cin.ignore();
+
+	char str3[50];
+
+
+	cout << "Please enter a string up to 49 characters: ";
+	cin.getline(str3, 49);
+	printf("%s\n", str3);
+
 
 
 

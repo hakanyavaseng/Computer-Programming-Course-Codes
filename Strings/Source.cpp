fe0be61a7437c3cdfcbcf8c9f48b4a45766bdf64
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-15 12:59:34 +0300

    Prints out string.

--- Strings/Source.cpp
+++ Strings/Source.cpp
@@ -6,6 +6,15 @@ using namespace std;
 int main(void)
 {
 
+	char str1[] = "This is my first string";
+
+	// Stringi print etmek!
+
+	puts(str1); // Otomatik olarak boþluk ekler.
+	printf("%s\n", str1); // Alta geçmesi için new line gerekiyor
+	cout << str1; // new line atmak gerekiyor
+
+
 
 
 

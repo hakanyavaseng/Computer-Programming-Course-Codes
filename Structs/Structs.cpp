dd23bb24c6d304d76fc7c0d024ddeeeed6656488
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-16 12:09:12 +0300

    Struct initilization, memory alignment.

--- Structs/Structs.cpp
+++ Structs/Structs.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+struct Obj
+{	
+	int en; // 4 bytes
+	int boy; // 4 bytes
+	char c; // 1 byte 
+
+	// Total 12 bytes, because of alignment
+
+};
+
+
+
 int main(void)
 {
+	Obj	myObject;
 
+	myObject.boy = 10;
+	myObject.en = 20;
+
+	cout << "Boy: " << myObject.boy << ", En: " << myObject.en << endl;
+	cout << "Size of struct: " << sizeof(myObject) << " bytes." << endl;
 
 
 

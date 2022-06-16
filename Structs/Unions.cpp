233ea05f856fc619892a4ce3b33dc83ee33ac726
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-16 13:09:52 +0300

    Union usage.

--- Structs/Unions.cpp
+++ Structs/Unions.cpp
@@ -5,33 +5,63 @@ using namespace std;
 struct Shape
 {
 	int h;
-	int x;
-	int y;
-	double r;
+
+	union {
+		struct 
+		{
+			int x;
+			int y;
+
+		};
+		struct 
+		{
+			double r;
+		};
+	};
+	
 
 };
 
+union testu
+{
+	int one;
+	int two;
+	char ch;
+};
+
+
 int main(void)
 {
+	//Complex array definitions
+
+
 	Shape rp;
 	rp.h = 10;
 	rp.x = 5;
 	rp.y = 2;
-	rp.r = 100;
 
 
 	cout << "Heigth: " << rp.h << ", X: " << rp.x << ", Y: " << rp.y << endl;
 	cout << "Volume: " << rp.h * rp.x * rp.y << endl;
 	cout << "Size of struct: " << sizeof(rp) << endl; // 24 basýyor
 
-	Shape cyc;
-
-	cyc.h = 8;
-	cyc.r = 20; 
 	
-	cout << "Heigth: " << cyc.h << ", r: " << cyc.r << endl;
-	cout << "Volume: " << cyc.h* (cyc.r*cyc.r) << endl;
-	cout << "Size of struct: " << sizeof(rp) << endl;
+
+
+	// Union en büyük üyenin boyutuna göre yer ayýrýr, hangi üye çaðrýlýrsa 
+	// ona göre deðerlendirme yapýyor. Hoca dedi :D
+
+	testu myu;
+
+	myu.one = 65;
+	
+	cout << "myu.one: " << myu.one << endl;
+	cout << "myu.two: " << myu.two << endl;
+	cout << "myu.ch: " << myu.ch << endl;
+
+	cout << "&myu.one: " << &myu.one << endl;
+	cout << "&myu.two: " << &myu.two << endl;
+	printf("&myu.ch = %p\n", &myu.ch);
 
 
 

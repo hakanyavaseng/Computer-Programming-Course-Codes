b855e299c54ae17eafba8242fba97d89b99ee20c
Hakan Yavaş <hakanyavaseng@gmail.com>
2022-06-16 12:38:08 +0300

    Complex structures.

--- Structs/Structs.cpp
+++ Structs/Structs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct Obj
@@ -11,7 +12,28 @@ struct Obj
 
 };
 
+// Complex structure definitions
 
+struct Student
+{
+	string name;
+	string surname;
+	string department;
+	string classChar;
+	string lesson;
+
+
+};
+
+struct School
+{
+	string schoolName;
+	string city;
+	string province;
+	string neighbour;
+
+	struct Student student[5];
+};
 
 int main(void)
 {
@@ -33,6 +55,42 @@ int main(void)
 	cout << "Using pointer: " << endl;
 	cout << "Boy: " << myObject.boy << ", En: " << myObject.en << endl;
 	cout << "Size of pointer: " << sizeof(myPtr) << " bytes." << endl;
+
+	// Array of structure
+
+	Obj* myArray = new struct Obj[5];
+
+	myArray[0].boy = 30;
+	myArray[0].en = 50;
+
+	myArray[1].boy = 60;
+	myArray[1].en = 100;
+
+	for (int i = 0; i < 2; i++)
+	{
+		cout << "Object " << i + 1 << ", Boy: " << myArray[i].boy << ", En: " << myArray[i].en << endl;
+	}
+
+	// Complex structure definitions
+
+	struct School University;
+
+	University.schoolName = "ESOGU";
+	University.city = "Eskisehir";
+	University.province = "Odunpazari";
+	University.neighbour = "Buyukdere";
+
+	University.student[0].name = "Hakan";
+	University.student[0].surname = "Yavas";
+	University.student[0].department = "CENG";
+	University.student[0].lesson = "Computer Programming";
+	University.student[0].classChar = "A";
+
+	cout << "Student information: " << endl;
+	cout << University.schoolName << "(" << University.city << ", " << University.province << ", " << University.neighbour << ")" << endl;
+	cout << University.student[0].name << " " << University.student[0].surname << " " << University.student[0].department << " " << University.student[0].lesson << " " << University.student[0].classChar << endl;
+
+
 
 
 
